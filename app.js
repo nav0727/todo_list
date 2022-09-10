a0c0f430ee396c6e2acaf66670686b5437d593d6
nav2707 <dosaninaveenreddy@gmail.com>
2022-09-10 13:06:35 +0000

    firstcommit

--- app.js
+++ app.js
@@ -24,23 +24,152 @@ const InitializeDbAndServer = async () => {
 };
 InitializeDbAndServer();
 
-//convert DBObj  to Res Obj
+//having or Not in query
 
-const convertDbToResObj = (dbObj) => {
-  return {
-    id: dbObj.id,
-    todo: dbObj.todo,
-    priority: dbObj.priority,
-    status: dbObj.status,
-  };
+const havingPriority = (requestQuery) => {
+  return requestQuery.priority !== undefined;
+};
+
+const havingStatus = (requestQuery) => {
+  return requestQuery.status !== undefined;
+};
+
+const havingStatusAndPriority = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  );
 };
 
 //get all TODO API
 
 app.get("/todos/", async (request, response) => {
-  const getQuery = `SELECT * FROM todo;`;
-  const todoArray = await database.all(getQuery);
-  response.send(todoArray.map((each) => convertDbToResObj(each)));
+  let todoArray = null;
+  let getTodoQuery = "";
+  const { search_q = "", priority, status } = request.query;
+
+  switch (true) {
+    case havingStatusAndPriority(request.query):
+      getTodoQuery = `
+      SELECT
+       * 
+       FROM 
+       todo
+        WHERE todo LIKE '%${search_q}%' 
+        AND status= '${status}' 
+        AND priority='${priority}';`;
+      break;
+    case havingPriority(request.query):
+      getTodoQuery = `
+      SELECT 
+      * 
+      FROM
+       todo
+        WHERE 
+        todo LIKE '%${search_q}%' 
+        AND  priority='${priority}';`;
+      break;
+    case havingStatus(request.query):
+      getTodoQuery = `
+      SELECT
+       *
+         FROM 
+       todo
+        WHERE todo LIKE '%${search_q}%'
+         AND status='${status}';`;
+      break;
+    default:
+      getTodoQuery = `
+      SELECT 
+      * 
+      FROM 
+      todo
+      WHERE
+        todo LIKE '%${search_q}%';`;
+  }
+
+  todoArray = await database.all(getTodoQuery);
+  response.send(todoArray);
+});
+
+// get Id by Todo API
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoQuery = `
+  SELECT 
+  *
+   FROM 
+   todo 
+   WHERE id=${todoId};`;
+  const todoSingle = await database.get(getTodoQuery);
+  response.send(todoSingle);
+});
+
+// Post Create todo API
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status } = request.body;
+  const postTodoQuery = `
+  INSERT INTO
+   todo(id,todo,priority,status)
+    VALUES (${id},'${todo}','${priority}','${status}');`;
+  await database.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+// Update put API
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let updatedValue = "";
+  const requestBodyValue = request.body;
+  switch (true) {
+    case requestBodyValue.status !== undefined:
+      updatedValue = "Status";
+      break;
+    case requestBodyValue.priority !== undefined:
+      updatedValue = "Priority";
+      break;
+    case requestBodyValue.todo !== undefined:
+      updatedValue = "Todo";
+      break;
+  }
+  const updateBeforeTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE 
+      id = ${todoId};`;
+  const beforeTodo = await database.get(updateBeforeTodoQuery);
+
+  const {
+    todo = beforeTodo.todo,
+    priority = beforeTodo.priority,
+    status = beforeTodo.status,
+  } = request.body;
+
+  const UpdateTodoQuery = `
+    UPDATE
+    todo
+    SET
+      todo='${todo}',
+      priority='${priority}',
+      status='${status}'
+    WHERE
+      id = ${todoId};`;
+
+  await database.run(UpdateTodoQuery);
+  response.send(`${updatedValue} Updated`);
+});
+
+// delete todo by Id API
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `DELETE FROM todo WHERE id=${todoId};`;
+  await database.run(deleteTodoQuery);
+  response.send("Todo Deleted");
 });
 
 module.exports = app;
